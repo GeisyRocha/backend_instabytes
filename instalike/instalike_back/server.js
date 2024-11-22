b763c4b4e4f666ec1a0f2e9c20add924ee01620d
GeisyRocha <geisyrocha17@gmail.com>
2024-11-22 16:36:38 -0400

    aula02

--- instalike/instalike_back/server.js
+++ instalike/instalike_back/server.js
@@ -1,13 +1,40 @@
 import express from "express"; // importando o express do pacote express, essa linha esta importando para o node_modules
 
+const posts = [
+    {id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/500"
+    },
+    {id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/500"
+    },
+    {id: 3, descricao: "Gato fazendo Comidinha", imagem: "https://placecats.com/millie/300/500"
+    },
+    
+  ];
+
+
 //usando o express
 
 const app = express();//express é uma função e vc vai usar apartir de app
+
+app.use = (express.json());
+
 app.listen(3000, () => {
     console.log("servidor escutando... ");
 } );        //funções recebem parametros
 
 //criando um rota
-app.get("/api", (req, res) => {
-    res.status(200).send("A torre Eifell iluminada à noite, com milhares de luzes cinitilando, criando um espetáculo mágico em Paris."); //
+app.get("/posts", (req, res) => {  //end point 
+    res.status(200).json(posts); //
+}) //requisição e resposta
+
+//buscando um dado dentro de uma estrutura
+function buscaPostID(id){
+    return posts.findIndex((post) =>{
+        return post.id === Number(id)
+
+    })
+}
+
+app.get("/posts/:id", (req, res) => {  //dado variavel
+    const index = buscaPostID(req.params.id)
+    res.status(200).json(posts[index]); //posição do post a ser retornado
 }) //requisição e resposta
